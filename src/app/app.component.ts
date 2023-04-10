61fcad49204d3cb08271adc954402e0266ffba0f
davidsan <david.sanluis@qrsof.com>
2023-04-10 12:19:07 -0600

    tabs component, isOnLoginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  private currentRoute: string = "";
+  isOnLoginPage = true;
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+        console.log(this.currentRoute); // Muestra la ruta actual en la consola
+        this.isOnLoginPage = this.currentRoute === "/" || this.currentRoute === "/login";
+      }
+    });
+
+
+  }
 }
