c258c9f5a98b201a8fedc9a4c4845454f0d5964e
davidsan <david.sanluis@qrsof.com>
2023-04-17 16:44:16 -0600

    ActionSheet

--- src/app/pages/new-card/new-card.page.ts
+++ src/app/pages/new-card/new-card.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
   selector: 'app-new-card',
@@ -7,8 +8,52 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./new-card.page.scss'],
 })
 export class NewCardPage implements OnInit {
+  selectedIcon = ""
+  isOnSelector = true
   id: number = 1
-  constructor(private route: ActivatedRoute) {
+
+  actionSheetButtons = [
+    {
+      icon: "person-circle",
+      text: 'name',
+      data: {
+        action: 'Name',
+        icon: "person-circle"
+      },
+    },
+    {
+      icon: 'call',
+      text: 'phone number',
+      data: {
+        action: 'Phone number',
+        icon: 'call',
+      },
+    },
+    {
+      icon: 'mail',
+      text: 'email',
+      data: {
+        action: 'Email',
+        icon: 'mail',
+      },
+    },
+    {
+      icon: 'location',
+      text: 'location',
+      data: {
+        action: 'Location',
+        icon: 'location',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ]
+  constructor(private route: ActivatedRoute, private actionSheetCtrl: ActionSheetController) {
   }
 
   ngOnInit() {
@@ -17,4 +62,22 @@ export class NewCardPage implements OnInit {
     })
   }
 
+  async presentActionSheet() {
+    this.selectedIcon = ''
+    this.isOnSelector = !this.isOnSelector
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: this.actionSheetButtons,
+    });
+
+    await actionSheet.present();
+    await actionSheet.onDidDismiss().then((data) => {
+      this.selectedIcon = data.data.icon
+      console.log(data)
+    })
+  }
+
+  onAddField(){
+    this.isOnSelector = !this.isOnSelector
+  }
 }
